f12c0ee230f76bc5c4d8454afaf5fc20915302c8
Patrick <patricklee0221@gmail.com>
2022-08-01 18:24:15 +0100

    implement heroBanner. take content from sanity

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
-const Home = () => {
+import { Product, FooterBanner, HeroBanner } from '../components';
+import { client } from '../lib/client';
+
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      HeroBanner
-      <div>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{['Product 1', 'Product 2'].map((product) => product)}</div>
-      Footer
+      <div className="products-container">
+        {products?.map((product) => product.name)}
+      </div>
+      <FooterBanner></FooterBanner>
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
